fa166ecd912d3bcd18020ca4a20b6f9499b35d56
bycho91 <bycho1991@gmail.com>
2020-09-30 23:59:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+import { Link, Router } from "@reach/router";
+import Details from "./Details";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Link to="/">
+          <h1>Adopt Me!</h1>
+        </Link>
       </header>
+
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
